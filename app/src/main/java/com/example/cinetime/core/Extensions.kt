4059bf70e3f54041b174cd9f82c0b3412ba621d8
Vitor <sblvitor@gmail.com>
2023-02-06 01:38:21 -0400

    implementaçao do core package

---
+++ app/src/main/java/com/example/cinetime/core/Extensions.kt
@@ -0,0 +1,34 @@
+package com.example.cinetime.core
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.example.cinetime.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun Fragment.createDialog(block: MaterialAlertDialogBuilder.() -> Unit = {}): androidx.appcompat.app.AlertDialog {
+    val builder = activity?.let { MaterialAlertDialogBuilder(it) }
+    builder!!.setPositiveButton(android.R.string.ok, null)
+    block(builder)
+    return builder.create()
+}
+
+fun Fragment.createProgressDialog(): AlertDialog {
+    return createDialog {
+        val padding = this@createProgressDialog.resources.getDimensionPixelOffset(R.dimen.margin_default)
+        val progressBar = ProgressBar(activity)
+        progressBar.setPadding(padding, padding, padding, padding)
+        setView(progressBar)
+
+        setPositiveButton(null, null)
+        setCancelable(false)
+    }
+}
+
+fun View.hideSoftKeyboard() {
+    val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
